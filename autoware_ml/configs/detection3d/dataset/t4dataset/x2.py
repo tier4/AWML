508899d0d30d757b7d490f6dc0baaadcbbcd47a5
Satoshi Tanaka <16330533+scepter914@users.noreply.github.com>
2024-06-25 08:22:21 +0900

    fix(autoware-ml): fix X2 config (#61)
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- autoware_ml/configs/detection3d/dataset/t4dataset/x2.py
+++ autoware_ml/configs/detection3d/dataset/t4dataset/x2.py
@@ -1,16 +1,26 @@
 custom_imports = dict(imports=[
-    "autoware_ml.detection3d.evaluation.t4metric",
     "autoware_ml.detection3d.datasets.t4dataset",
+    "autoware_ml.detection3d.evaluation.t4metric.t4metric",
 ])
 
+# dataset type setting
+dataset_type = "T4Dataset"
+info_train_file_name = "t4dataset_x2_infos_train.pkl"
+info_val_file_name = "t4dataset_x2_infos_val.pkl"
+info_test_file_name = "t4dataset_x2_infos_test.pkl"
+
+# dataset scene setting
 dataset_version_config_root = "autoware_ml/configs/detection3d/dataset/t4dataset/"
 dataset_version_list = ["database_v2_0", "database_v3_0"]
 
+# dataset format setting
+data_prefix = dict(pts="", sweeps="")
 camera_types = {
     "CAM_FRONT", "CAM_FRONT_RIGHT", "CAM_FRONT_LEFT", "CAM_BACK",
     "CAM_BACK_LEFT", "CAM_BACK_RIGHT"
 }
 
+# class setting
 name_mapping = {
     "animal": "animal",
     "movable_object.barrier": "barrier",
