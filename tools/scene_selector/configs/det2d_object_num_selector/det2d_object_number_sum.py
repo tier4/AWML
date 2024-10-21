8c5d30ff0dbb2425783855fb50847acb7bf3d2f8
Samrat Thapa <38401989+SamratThapa120@users.noreply.github.com>
2024-10-21 22:27:14 +0900

    feat(scene_selector): GLIP and YOLOX scene selector (#161)
    Co-authored-by: Satoshi Tanaka <16330533+scepter914@users.noreply.github.com>

--- tools/scene_selector/configs/det2d_object_num_selector/det2d_object_number_sum.py
+++
@@ -1,7 +0,0 @@
-scene_selector = dict(
-    type = "Det2dObjectNumSelector",
-    model_type = "YOLOX",
-    confidence_threshold = 0.5,
-    target_label = ["car", "bicycle", "pedestrian", "truck", "bus"],
-    threshold_object_num = 20,
-)
--- tools/scene_selector/configs/det2d_object_num_selector/det2d_target_label.py
+++
@@ -1,7 +0,0 @@
-scene_selector = dict(
-    type = "Det2dObjectNumSelector",
-    model_type = "YOLOX",
-    confidence_threshold = 0.5,
-    target_label = ["bicycle"],
-    threshold_object_num = 1,
-)
