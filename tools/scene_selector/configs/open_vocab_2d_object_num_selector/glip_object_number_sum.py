9f4ba434fa90649d5d0e02758d75d6219160cf5e
Satoshi Tanaka <16330533+scepter914@users.noreply.github.com>
2024-12-12 17:31:58 +0900

    fix(scene_selector): apply formatter (#316)
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- tools/scene_selector/configs/open_vocab_2d_object_num_selector/glip_object_number_sum.py
+++ tools/scene_selector/configs/open_vocab_2d_object_num_selector/glip_object_number_sum.py
@@ -1,12 +1,10 @@
-t4_dataset_sensor_names = ['CAM_FRONT','CAM_BACK']
+t4_dataset_sensor_names = ["CAM_FRONT", "CAM_BACK"]
 batch_size = 8
 
 scene_selector = dict(
     type="OpenVocab2dObjectNumSelector",
-    model_config_path=
-    "/workspace/projects/GLIP/configs/glip_atss_swin-l_fpn_dyhead_pretrain_mixeddata.py",
-    model_checkpoint_path=
-    "https://download.openmmlab.com/mmdetection/v3.0/glip/glip_l_mmdet-abfe026b.pth",
+    model_config_path="/workspace/projects/GLIP/configs/glip_atss_swin-l_fpn_dyhead_pretrain_mixeddata.py",
+    model_checkpoint_path="https://download.openmmlab.com/mmdetection/v3.0/glip/glip_l_mmdet-abfe026b.pth",
     confidence_threshold=0.6,
     target_and_threshold={
         "traffic cone": 1,
@@ -16,5 +14,5 @@ scene_selector = dict(
         "baby stroller": 1,
         "articulated bus": 1,
     },
-    batch_size=batch_size
+    batch_size=batch_size,
 )
