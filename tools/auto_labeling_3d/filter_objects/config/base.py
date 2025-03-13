4f6cd9c90e433b20d1a336e69cf35e14aeab6aa0
Shin-kyoto <aquashin0202@gmail.com>
2025-03-13 19:34:44 +0900

    feat(auto_labeling_3d): add filter_objects
    Signed-off-by: Shin-kyoto <aquashin0202@gmail.com>

--- tools/auto_labeling_3d/filter_objects/config/base.py
+++ tools/auto_labeling_3d/filter_objects/config/base.py
@@ -1,14 +1,53 @@
-filter_pipelines = EnsembleModel(
-    [
-        ThresholdFilter(
+custom_imports = dict(
+    imports=["tools.auto_labeling_3d.filter_objects.filter", "tools.auto_labeling_3d.filter_objects.ensemble"],
+    allow_failed_imports=False,
+)
+
+centerpoint_pipeline = [
+    dict(
+        type="ThresholdFilter",
+        confidence_thresholds={
+            "car": 0.35,
+            "truck": 0.35,
+            "bus": 0.35,
+            "bicycle": 0.35,
+            "pedestrian": 0.10,
+        },
+        use_label=["car", "truck", "bus", "bicycle", "pedestrian"],
+    ),
+]
+
+bevfusion_pipeline = [
+    dict(
+        type="ThresholdFilter",
+        confidence_thresholds={
+            "car": 0.35,
+            "truck": 0.35,
+            "bus": 0.35,
+            "bicycle": 0.35,
+            "pedestrian": 0.10,
+        },
+        use_label=["car", "truck", "bus", "bicycle", "pedestrian"],
+    ),
+]
+
+filter_pipelines = dict(
+    type="EnsembleModel",
+    models=[
+        dict(
+            name="centerpoint",
             info_path="./data/t4dataset/info/pseudo_infos_raw_centerpoint.pkl",
-            confidence_threshold=0.40,
-            use_label=["car", "track", "bus", "bike", "pedestrian"],
+            filter_pipeline=centerpoint_pipeline,
         ),
-        ThresholdFilter(
+        dict(
+            name="bevfusion",
             info_path="./data/t4dataset/info/pseudo_infos_raw_bevfusion.pkl",
-            confidence_threshold=0.25,
-            use_label=["bike", "pedestrian"],
+            filter_pipeline=bevfusion_pipeline,
         ),
-    ]
+    ],
+    ensemble_config=dict(
+        weights=[1.0, 1.0],
+        iou_threshold=0.55,
+        skip_box_threshold=0.0,
+    ),
 )
--- tools/auto_labeling_3d/filter_objects/filter_objects.py
+++ tools/auto_labeling_3d/filter_objects/filter_objects.py
@@ -1,1 +1,102 @@
-# [TODO] implement
+import argparse
+import logging
+import pickle
+from pathlib import Path
+from typing import Any, Dict, List
+
+from mmengine.config import Config
+from mmengine.registry import TASK_UTILS, init_default_scope
+
+from tools.auto_labeling_3d.utils.logger import setup_logger
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Filter objects which do not use for pseudo T4dataset from pseudo labeled info file"
+    )
+
+    parser.add_argument("--config", type=str, required=True, help="Path to config file of the filtering")
+    parser.add_argument(
+        "--work-dir", required=True, help="the directory to save the file containing evaluation metrics"
+    )
+    parser.add_argument(
+        "--log-level",
+        help="Set log level",
+        default="INFO",
+        choices=list(logging._nameToLevel.keys()),
+    )
+    return parser.parse_args()
+
+
+def filter_results(filter_cfg, predicted_result_info, predicted_result_info_name) -> Dict[str, Any]:
+    """
+    Args:
+        filter_cfg (Dict[str, Any]): config for filter pipeline.
+        predicted_result_info (Dict[str, Any]): Info dict that contains predicted result.
+        predicted_result_info_name (str): Name of info dict.
+
+    Returns:
+        Dict[str, Any]: Filtered info dict
+    """
+    filter_model = TASK_UTILS.build(filter_cfg)
+    return filter_model.filter(predicted_result_info, predicted_result_info_name)
+
+
+def ensemble_results(ensemble_cfg: Dict[str, Any]):
+    """
+    Args:
+        ensemble_cfg (Dict[str, Any]): config for ensemble.
+
+    Returns:
+        Dict[str, Any]: Ensembled info dict
+    """
+    ensemble_model = TASK_UTILS.build(ensemble_cfg.filter_pipelines)
+    results: List[Dict[str, Any]] = []
+    names = []
+    for model_config in ensemble_model.models:
+        names.append(model_config["name"])
+
+        # load info file
+        with open(model_config["info_path"], "rb") as f:
+            info = pickle.load(f)
+
+        # apply filters in pipelines
+        for filter_cfg in model_config["filter_pipeline"]:
+            info = filter_results(filter_cfg, info, model_config["info_path"])
+
+        results.append(info)
+
+    name = "+".join(names)
+    output_info: Dict[str, Any] = ensemble_model.ensemble(results)
+    return name, output_info
+
+
+def main():
+    init_default_scope("mmdet3d")
+
+    args = parse_args()
+
+    # Load config
+    cfg = Config.fromfile(args.config)
+
+    logger: logging.Logger = setup_logger(args, name="filter_objects")
+
+    # Apply filtering
+    logger.info("Filtering objects...")
+    if cfg.filter_pipelines.type == "EnsembleModel":
+        name, output_info = ensemble_results(cfg)
+    else:
+        raise ValueError(f"Unknown filter type: {cfg.filter_pipelines.type}")
+
+    # Get input info path and determine output path
+    output_path = Path(args.work_dir) / f"pseudo_infos_{name}_filtered.pkl"
+
+    # Save filtered results
+    logger.info(f"Saving filtered results to {output_path}")
+    with open(output_path, "wb") as f:
+        pickle.dump(output_info, f)
+    logger.info("Finish filtering")
+
+
+if __name__ == "__main__":
+    main()
