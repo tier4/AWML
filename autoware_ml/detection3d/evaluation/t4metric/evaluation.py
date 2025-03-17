21b8f31a806cac27bb6b37382ef30931013b81bc
kokseang <kokseang.tan@tier4.jp>
2025-03-17 18:13:50 +0900

    Add initial evaluation configs

--- autoware_ml/detection3d/evaluation/t4metric/evaluation.py
+++ autoware_ml/detection3d/evaluation/t4metric/evaluation.py
@@ -2,7 +2,6 @@ import json
 import os
 from typing import Any, Dict, List, Tuple
 
-from mmengine.registry import TASK_UTILS
 from nuscenes.eval.common.data_classes import EvalBoxes
 from nuscenes.eval.detection.data_classes import DetectionConfig
 from nuscenes.eval.detection.evaluate import DetectionEval
