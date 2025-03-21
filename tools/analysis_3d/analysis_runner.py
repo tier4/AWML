20ca4a7486a000bb34ec008e1e84f1db2be2776c
kokseang <kokseang.tan@tier4.jp>
2025-03-21 18:02:53 +0900

    Added voxel num analyses

--- tools/analysis_3d/analysis_runner.py
+++ tools/analysis_3d/analysis_runner.py
@@ -9,10 +9,19 @@ from t4_devkit import Tier4
 from tools.analysis_3d.callbacks.callback_interface import AnalysisCallbackInterface
 from tools.analysis_3d.callbacks.category import CategoryAnalysisCallback
 from tools.analysis_3d.callbacks.category_attribute import CategoryAttributeAnalysisCallback
-from tools.analysis_3d.data_classes import AnalysisData, DatasetSplitName, SampleData, ScenarioData
+from tools.analysis_3d.callbacks.voxel_num import VoxelNumAnalysisCallback
+from tools.analysis_3d.data_classes import (
+    AnalysisData,
+    DatasetSplitName,
+    LidarPoint,
+    LidarSweep,
+    SampleData,
+    ScenarioData,
+)
 from tools.analysis_3d.split_options import SplitOptions
 from tools.analysis_3d.utils import extract_tier4_sample_data
 from tools.detection3d.create_data_t4dataset import get_scene_root_dir_path
+from tools.detection3d.t4dataset_converters.t4converter import get_lidar_points_info, get_lidar_sweeps_info
 
 
 class AnalysisRunner:
@@ -23,6 +32,7 @@ class AnalysisRunner:
         data_root_path: str,
         config_path: str,
         out_path: str,
+        max_sweeps: int = 10,
     ) -> None:
         """
         :param data_root_path: Path where to save data.
@@ -36,29 +46,37 @@ class AnalysisRunner:
         self.config = Config.fromfile(self.config_path)
         self.out_path.mkdir(parents=True, exist_ok=True)
         self.remapping_classes = self.config.name_mapping
+        self.max_sweeps = max_sweeps
 
         # Default callbacks to generate analyses
         # TODO (KokSeang): Configure through CLI
         self.analysis_callbacks: List[AnalysisCallbackInterface] = [
-            CategoryAnalysisCallback(out_path=self.out_path, remapping_classes=self.remapping_classes),
-            CategoryAttributeAnalysisCallback(
-                out_path=self.out_path, category_name="vehicle.motorcycle", analysis_dir="vehicle_motorcycle_attr"
-            ),
-            CategoryAttributeAnalysisCallback(
-                out_path=self.out_path, category_name="vehicle.bicycle", analysis_dir="vehicle_bicycle_attr"
-            ),
-            CategoryAttributeAnalysisCallback(
-                out_path=self.out_path, category_name="bicycle", analysis_dir="bicycle_attr"
-            ),
-            CategoryAttributeAnalysisCallback(
-                out_path=self.out_path, category_name="motorcycle", analysis_dir="motorcycle_attr"
-            ),
-            CategoryAttributeAnalysisCallback(
+            VoxelNumAnalysisCallback(
                 out_path=self.out_path,
-                category_name="bicycle",
-                analysis_dir="remapping_bicycle_attr",
-                remapping_classes=self.remapping_classes,
+                pc_ranges=[-121.60, -121.60, -3.0, 121.60, 121.60, 5.0],
+                voxel_sizes=[0.32, 0.32, 8.0],
+                analysis_dir="voxel_nums",
+                bins=100,
             ),
+            # CategoryAnalysisCallback(out_path=self.out_path, remapping_classes=self.remapping_classes),
+            # CategoryAttributeAnalysisCallback(
+            #     out_path=self.out_path, category_name="vehicle.motorcycle", analysis_dir="vehicle_motorcycle_attr"
+            # ),
+            # CategoryAttributeAnalysisCallback(
+            #     out_path=self.out_path, category_name="vehicle.bicycle", analysis_dir="vehicle_bicycle_attr"
+            # ),
+            # CategoryAttributeAnalysisCallback(
+            #     out_path=self.out_path, category_name="bicycle", analysis_dir="bicycle_attr"
+            # ),
+            # CategoryAttributeAnalysisCallback(
+            #     out_path=self.out_path, category_name="motorcycle", analysis_dir="motorcycle_attr"
+            # ),
+            # CategoryAttributeAnalysisCallback(
+            #     out_path=self.out_path,
+            #     category_name="bicycle",
+            #     analysis_dir="remapping_bicycle_attr",
+            #     remapping_classes=self.remapping_classes,
+            # ),
         ]
 
     def _get_dataset_scenario_names(self, dataset_version: str) -> Dict[str, List[str]]:
@@ -82,9 +100,39 @@ class AnalysisRunner:
             # Extract sample data
             tier4_sample_data = extract_tier4_sample_data(sample=sample, t4=t4)
 
+            lidar_point_info = get_lidar_points_info(
+                lidar_path=tier4_sample_data.lidar_path,
+                cs_record=tier4_sample_data.cs_record,
+                num_features=5,
+            )
+            lidar_point = LidarPoint(
+                num_pts_feats=lidar_point_info["num_pts_feats"],
+                lidar_path=lidar_point_info["lidar_path"],
+                lidar2ego=lidar_point_info["lidar2ego"],
+            )
+
+            lidar_sweep_info = get_lidar_sweeps_info(
+                t4=t4,
+                cs_record=tier4_sample_data.cs_record,
+                sd_rec=tier4_sample_data.sd_record,
+                pose_record=tier4_sample_data.pose_record,
+                num_features=5,
+                max_sweeps=self.max_sweeps,
+            )
+            lidar_sweeps = [
+                LidarSweep(
+                    num_pts_feats=lidar_sweep["lidar_points"]["num_pts_feats"],
+                    lidar_path=lidar_sweep["lidar_points"]["lidar_path"],
+                )
+                for lidar_sweep in lidar_sweep_info
+            ]
+
             # Convert to SampleData
             sample_data[sample.token] = SampleData.create_sample_data(
-                sample_token=sample.token, boxes=tier4_sample_data.boxes
+                sample_token=sample.token,
+                boxes=tier4_sample_data.boxes,
+                lidar_point=lidar_point,
+                lidar_sweeps=lidar_sweeps,
             )
         return sample_data
 
---
+++ tools/analysis_3d/callbacks/voxel_num.py
@@ -0,0 +1,162 @@
+from collections import defaultdict
+from pathlib import Path
+from typing import Dict, List, Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+from mmengine.logging import print_log
+
+from tools.analysis_3d.callbacks.callback_interface import AnalysisCallbackInterface
+from tools.analysis_3d.data_classes import AnalysisData, DatasetSplitName, LidarPoint, LidarSweep
+from tools.analysis_3d.split_options import SplitOptions
+
+
+class VoxelNumAnalysisCallback(AnalysisCallbackInterface):
+    """Compute number of voxels for every pointcloud and its multiple sweeps."""
+
+    def __init__(
+        self,
+        out_path: Path,
+        pc_ranges: List[float],
+        voxel_sizes: List[float],
+        load_dim: int = 5,
+        use_dim: List[int] = [0, 1, 2],
+        sweeps_num: int = 1,
+        remove_close: bool = True,
+        analysis_dir: str = "voxel_nums",
+        bins: int = 100,
+    ) -> None:
+        """
+        :param out_path: Path to save outputs.
+        :param analysis_dir: Folder name to save outputs.
+        :param remapping_classes: Set if compute frequency of every category after remapping.
+        """
+        super(AnalysisCallbackInterface, self).__init__()
+        self.out_path = out_path
+        self.pc_ranges = pc_ranges
+        self.voxel_sizes = voxel_sizes
+        self.analysis_dir = analysis_dir
+        self.load_dim = load_dim
+        self.use_dum = use_dim
+        self.sweeps_num = sweeps_num
+        self.remove_close = remove_close
+        self.full_output_path = self.out_path / self.analysis_dir
+        self.full_output_path.mkdir(exist_ok=True, parents=True)
+
+        self.analysis_file_name = "voxel_count_{}_{}_{}.png"
+        self.y_axis_label = "Number of voxels"
+        self.x_axis_label = "Bins"
+        self.legend_loc = "upper right"
+        self.bins = bins
+
+    def _remove_close(self, points: npt.NDArray[np.float32], radius: float = 1.0) -> npt.NDArray[np.float32]:
+        """Remove point too close within a certain radius from origin.
+
+        Args:
+            points (np.ndarray | :obj:`BasePoints`): Sweep points.
+            radius (float): Radius below which points are removed.
+                Defaults to 1.0.
+
+        Returns:
+            np.ndarray | :obj:`BasePoints`: Points after removing.
+        """
+        x_filt = np.abs(points[:, 0]) < radius
+        y_filt = np.abs(points[:, 1]) < radius
+        not_close = np.logical_not(np.logical_and(x_filt, y_filt))
+        return points[not_close]
+
+    def _load_points(self, pcd_file: str) -> npt.NDArray[np.float32]:
+        """ """
+        return np.fromfile(pcd_file, dtype=np.float32).reshape(-1, self.load_dim)
+
+    def _load_multisweeps(self, points: npt.NDArray[np.float32], sweeps: List[LidarSweep]) -> npt.NDArray[np.float32]:
+        """ """
+        points = points[:, self.use_dim]
+        sweep_points_list = [points]
+
+        choices = np.random.choice(len(sweeps), self.sweeps_num, replace=False)
+
+        for idx in choices:
+            sweep: LidarSweep = sweeps[idx]
+            points_sweep = self._load_points(sweep.lidar_path)
+            if self.remove_close:
+                points_sweep = self._remove_close(points_sweep)
+                points_sweep = points_sweep[:, self.use_dim]
+                sweep_points_list.append(points_sweep)
+
+        points = np.concatenate(sweep_points_list, axis=0)
+        return points
+
+    def _get_total_voxel_counts(self, points: npt.NDArray[np.float64]) -> int:
+        """ """
+        # Normalize the points by dividing by voxel size
+        voxel_indices = np.floor(
+            (points - np.array([self.pc_ranges[0], self.pc_ranges[1], self.pc_ranges[2]])) / self.voxel_sizes
+        ).astype(np.int32)
+
+        # Remove duplicate voxels (points inside the same voxel)
+        unique_voxels = np.unique(voxel_indices, axis=0)
+
+        return len(unique_voxels)
+
+    def _visualize_dataset_voxel_counts(
+        self,
+        dataset_analysis_data: Dict[str, AnalysisData],
+        split_name: str,
+        log_scale: bool = False,
+        figsize: tuple[int, int] = (15, 15),
+    ) -> None:
+        """ """
+        for dataset_name, analysis_data in dataset_analysis_data.items():
+            self._visualize_voxel_counts(analysis_data, split_name, log_scale, figsize)
+
+    def _visualize_voxel_counts(
+        self,
+        analysis_data: AnalysisData,
+        split_name: str,
+        log_scale: bool = False,
+        figsize: tuple[int, int] = (15, 15),
+    ) -> None:
+        """ """
+        voxel_counts = []
+        for scenario_data in analysis_data.scenario_data.values():
+            for sample_data in scenario_data.sample_data.values():
+                if sample_data.lidar_point is None:
+                    continue
+
+                points = self._load_points(sample_data.lidar_point.lidar_path)
+                if sample_data.lidar_sweeps:
+                    points = self._load_multisweeps(points, sample_data.lidar_sweeps)
+
+            voxel_counts.append(self._get_total_voxel_counts(points))
+
+        _, ax = plt.subplots(figsize=figsize)
+        ax.hist(voxel_counts, bins=self.bins, log=log_scale)
+        ax.set_ylabel(self.y_axis_label)
+        ax.set_xlabel(self.x_axis_label)
+        ax.set_title(f"Voxel counts for {split_name} \n {self.pc_ranges} \n {self.voxel_sizes}")
+        plt.tight_layout()
+        analysis_file_name = self.full_output_path / self.analysis_file_name.format(
+            split_name, self.pc_ranges, self.voxel_sizes
+        )
+        plt.savefig(
+            fname=analysis_file_name,
+            format="png",
+            bbox_inches="tight",
+        )
+        plt.close()
+
+    def run(self, dataset_split_analysis_data: Dict[DatasetSplitName, AnalysisData]) -> None:
+        """Inherited, check the superclass."""
+        print_log(f"Running {self.__class__.__name__}")
+        for split_option in SplitOptions:
+            dataset_voxel_data = {}
+            for dataset_split_name, analysis_data in dataset_split_analysis_data.items():
+                split_name = dataset_split_name.split_name
+                if split_name != split_option.value:
+                    continue
+                dataset_voxel_data[dataset_split_name.dataset_version] = analysis_data
+
+            self._visualize_dataset_voxel_counts(dataset_analysis_data=dataset_voxel_data, split_name=split_name)
+        print_log(f"Done running {self.__class__.__name__}")
