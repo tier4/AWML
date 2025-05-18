da8b258eb362158e8254c6af948081457c498e60
Kenzo Lobos-Tsunekawa <kenokamikawa@gmail.com>
2025-05-18 22:25:58 +0900

    chore: delete unused assets
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenokamikawa@gmail.com>

--- projects/PCSegSAM2/grounding_dino/docker_test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import torch
-from groundingdino.util.inference import annotate, load_image, load_model, predict
-
-model = load_model("groundingdino/config/GroundingDINO_SwinT_OGC.pyy", "weights/groundingdino_swint_ogc.pth")
-model = model.to("cuda:0")
-print(torch.cuda.is_available())
-print("DONE!")
