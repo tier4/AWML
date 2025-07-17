92e115a7c8fffc991fdb5cce5758549852e4b500
Kok Seang <kseangtan@gmail.com>
2025-07-17 18:11:52 +0900

    Fix import

--- tools/detection3d/test.py
+++ tools/detection3d/test.py
@@ -7,6 +7,7 @@ from mmdet3d.utils import replace_ceph_backend
 from mmengine.config import Config, ConfigDict, DictAction
 from mmengine.registry import RUNNERS
 from mmengine.runner import Runner
+from mmengine.logging import print_log
 
 
 # TODO: support fuse_conv_bn and format_only
