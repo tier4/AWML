821095d5e9494e03e55711bb7ee95fbdb99c8c74
Kok Seang <kseangtan@gmail.com>
2025-09-09 12:56:09 +0900

    Add jpntaxi gen2 v2

--- autoware_ml/configs/detection3d/dataset/t4dataset/jpntaxi_gen2_base.py
+++ autoware_ml/configs/detection3d/dataset/t4dataset/jpntaxi_gen2_base.py
@@ -15,6 +15,7 @@ info_test_file_name = "t4dataset_jpntaxi_gen2_base_infos_test.pkl"
 dataset_version_config_root = "autoware_ml/configs/t4dataset/"
 dataset_test_groups = {
     "db_jpntaxi_gen2_base": "t4dataset_jpntaxi_gen2_base_infos_test.pkl",
+    "db_jpntaxi_gen2_v2": "t4dataset_jpntaxi_gen2_v2_infos_test.pkl",
 }
 
 dataset_version_list = [
