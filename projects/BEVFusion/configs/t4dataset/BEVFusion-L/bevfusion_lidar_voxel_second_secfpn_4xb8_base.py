0289c2db1ed5e94212978c55ea6527d7e7d1d5b1
KokSeang Tan <kseangtan@Gmail.com>
2025-12-07 06:25:04 +0900

    Rename config file name

--- projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_4xb8_base.py
+++ projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_4xb8_base.py
@@ -104,7 +104,7 @@ train_pipeline = [
         type="BEVFusionGlobalRotScaleTrans",
         scale_ratio_range=[0.95, 1.05],
         rot_range=[-0.78539816, 0.78539816],
-        translation_std=[0.2, 0.2, 0.2],
+        translation_std=[0.5, 0.5, 0.2],
     ),
     dict(type="BEVFusionRandomFlip3D"),
     dict(type="PointsRangeFilter", point_cloud_range=point_cloud_range),
