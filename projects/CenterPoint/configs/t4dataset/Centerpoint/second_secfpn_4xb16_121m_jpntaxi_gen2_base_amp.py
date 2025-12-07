0118c6b0cc6c73bf0338f4f62786aac0a8596672
Kok Seang Tan <49808895+KSeangTan@users.noreply.github.com>
2025-12-07 17:03:32 +0900

    feat(sampler): Implementation of Instance-Aware Repeat Factor Sampling (RFS) to CenterPoint 2.4.x (#105)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- projects/CenterPoint/configs/t4dataset/Centerpoint/second_secfpn_4xb16_121m_jpntaxi_gen2_base_amp.py
+++ projects/CenterPoint/configs/t4dataset/Centerpoint/second_secfpn_4xb16_121m_jpntaxi_gen2_base_amp.py
@@ -8,6 +8,7 @@ custom_imports["imports"] += _base_.custom_imports["imports"]
 custom_imports["imports"] += ["autoware_ml.detection3d.datasets.transforms"]
 custom_imports["imports"] += ["autoware_ml.hooks"]
 custom_imports["imports"] += ["autoware_ml.backends.mlflowbackend"]
+custom_imports["imports"] += ["autoware_ml.samplers"]
 
 # This is a base file for t4dataset, add the dataset config.
 # type, data_root and ann_file of data.train, data.val and data.test
@@ -48,7 +49,7 @@ test_batch_size = 2
 num_workers = 32
 val_interval = 1
 max_epochs = 30
-work_dir = "work_dirs/centerpoint/" + _base_.dataset_type + "/second_secfpn_4xb16_121m_jpntaxi_gen2_base/"
+work_dir = "work_dirs/centerpoint/" + _base_.dataset_type + "/second_secfpn_4xb16_121m_jpntaxi_gen2_base_amp"
 
 train_pipeline = [
     dict(
