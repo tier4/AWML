d7e2a1513500c6eac9005789cd33b4e09e56da79
Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>
2025-12-08 10:14:50 +0900

    feat(autoware_ml): adjust configuration
    Signed-off-by: Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>

--- autoware_ml/configs/segmentation3d/dataset/t4dataset/j6gen2_base.py
+++ autoware_ml/configs/segmentation3d/dataset/t4dataset/j6gen2_base.py
@@ -48,8 +48,6 @@ class_names = [
     "out_of_sync",
 ]
 
-num_class = len(class_names)
-
 metainfo = dict(
     classes=class_names,
 )
@@ -59,35 +57,35 @@ merge_type = None
 
 # visualization
 class_colors = {
-    "unknown": (0, 0, 0),  # Black
-    "driveable_surface": (0, 255, 255),  # Cyan / Aqua
-    "manmade": (233, 233, 229),  # Light Beige/Grey
-    "other_flat_surface": (110, 110, 110),  # Dark Grey
-    "vegetation": (0, 175, 0),  # Green
-    "sidewalk": (232, 35, 244),  # Magenta
-    "car": (255, 158, 0),  # Orange
-    "pedestrian": (0, 0, 230),  # Blue
-    "emergency_vehicle": (255, 0, 0),  # Bright Red
-    "truck": (255, 127, 80),  # Coral
-    "tractor_unit": (160, 60, 60),  # Brownish Red
-    "semi_trailer": (255, 140, 0),  # Dark Orange
-    "bus": (255, 215, 0),  # Gold
-    "bicycle": (220, 20, 60),  # Crimson
-    "motorcycle": (255, 61, 99),  # Reddish Pink
-    "construction_vehicle": (230, 230, 0),  # Yellow
-    "other_stuff": (128, 128, 128),  # Medium Grey
-    "noise": (255, 0, 255),  # Bright Fuchsia
-    "traffic_cone": (47, 79, 79),  # Dark Slate Grey
-    "debris": (139, 69, 19),  # Saddle Brown
-    "forklift": (218, 165, 32),  # Goldenrod
-    "kart": (128, 0, 128),  # Purple
-    "stroller": (135, 206, 235),  # Sky Blue
-    "personal_mobility": (0, 0, 128),  # Navy Blue
-    "pushable_pullable": (170, 170, 170),  # Light Grey
-    "animal": (205, 133, 63),  # Peru
-    "train": (255, 99, 71),  # Tomato Red
-    "ghost_point": (50, 50, 50),  # Very Dark Grey
-    "out_of_sync": (255, 255, 255),  # White
+    0: (0, 0, 0),  # Black
+    1: (0, 255, 255),  # Cyan / Aqua
+    2: (233, 233, 229),  # Light Beige/Grey
+    3: (110, 110, 110),  # Dark Grey
+    4: (0, 175, 0),  # Green
+    5: (232, 35, 244),  # Magenta
+    6: (255, 158, 0),  # Orange
+    7: (0, 0, 230),  # Blue
+    8: (255, 0, 0),  # Bright Red
+    9: (255, 127, 80),  # Coral
+    10: (160, 60, 60),  # Brownish Red
+    11: (255, 140, 0),  # Dark Orange
+    12: (255, 215, 0),  # Gold
+    13: (220, 20, 60),  # Crimson
+    14: (255, 61, 99),  # Reddish Pink
+    15: (230, 230, 0),  # Yellow
+    16: (128, 128, 128),  # Medium Grey
+    17: (255, 0, 255),  # Bright Fuchsia
+    18: (47, 79, 79),  # Dark Slate Grey
+    19: (139, 69, 19),  # Saddle Brown
+    20: (218, 165, 32),  # Goldenrod
+    21: (128, 0, 128),  # Purple
+    22: (135, 206, 235),  # Sky Blue
+    23: (0, 0, 128),  # Navy Blue
+    24: (170, 170, 170),  # Light Grey
+    25: (205, 133, 63),  # Peru
+    26: (255, 99, 71),  # Tomato Red
+    27: (240, 240, 240),  # Ghost White
+    28: (50, 50, 50),  # Very Dark Grey
 }
 
 filter_attributes = None
