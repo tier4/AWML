3d781937fb99415f98e0dd5e14a8e0831b1f6f08
tzhong518 <sworgun@gmail.com>
2025-12-12 12:49:21 +0900

    add: comlops dataset and ioumetrc
    Signed-off-by: tzhong518 <sworgun@gmail.com>

--- autoware_ml/detection2d/metrics/__init__.py
+++ autoware_ml/detection2d/metrics/__init__.py
@@ -1,3 +1,6 @@
 from .tlr_metrics import TLRFineDetectorEvaluator
+from mmseg.evaluation.metrics import IoUMetric
+from mmengine.registry import METRICS
+METRICS.register_module()(IoUMetric)
 
-__all__ = ["TLRFineDetectorEvaluator"]
+__all__ = ["TLRFineDetectorEvaluator", "IoUMetric"]
