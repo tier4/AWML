7be3dddaced14a606c11a4d2277573104dc48260
Kok Seang <kseangtan@gmail.com>
2025-12-16 22:14:25 +0900

    Add schedulers configs

--- projects/BEVFusion/configs/t4dataset/default/bevfusion_camera_second_secfpn_4xb8_centerhead_aux_120m.py
+++ projects/BEVFusion/configs/t4dataset/default/bevfusion_camera_second_secfpn_4xb8_centerhead_aux_120m.py
@@ -2,7 +2,7 @@ _base_ = [
     "bevfusion_lidar_intensity_voxel_second_secfpn_4xb8_120m.py",
     "./models/bevfusion_camera_second_secfpn.py"
     "./pipelines/default_camera_lidar_intensity_120m.py",
-    "./schedulers/default_30e_linear_cosine.py"
+    "./schedulers/default_50e_linear_cosine.py"
 ]
 
 model = dict(
