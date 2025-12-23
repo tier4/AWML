08a2a99a7ed444be57759ac03681637574b65bec
tzhong518 <sworgun@gmail.com>
2025-12-23 14:35:59 +0900

    add: yolox_multitask model
    Signed-off-by: tzhong518 <sworgun@gmail.com>

---
+++ projects/YOLOX_opt_elan/configs/t4dataset/YOLOX_opt-S-DynamicRecognition/yolox-s-opt-elan-semseg_960x960_300e_t4dataset.py
@@ -0,0 +1,372 @@
+_base_ = [
+    "../../../../../autoware_ml/configs/detection2d/default_runtime.py",
+    "../../../../../autoware_ml/configs/detection2d/schedules/schedule_1x.py",
+    "../../../../../autoware_ml/configs/detection2d/dataset/t4dataset/comlops.py",
+]
+
+custom_imports = dict(
+    imports=[
+        "projects.YOLOX_opt_elan.yolox",
+        "autoware_ml.detection2d.metrics",
+        "autoware_ml.detection2d.datasets",
+        "projects.YOLOX_opt_elan.yolox.models",
+        "projects.YOLOX_opt_elan.yolox.models.yolox_multitask",
+        "projects.YOLOX_opt_elan.yolox.transforms",
+    ],
+    allow_failed_imports=False,
+)
+
+IMG_SCALE = (960, 960)
+
+# parameter settings
+img_scale = (960, 960)
+max_epochs = 300
+num_last_epochs = 15
+resume_from = None
+interval = 1
+batch_size = 12
+activation = "ReLU6"
+num_workers = 4
+
+base_lr = 0.001
+
+# model settings
+model = dict(
+    type="YOLOXMultiTask",
+    data_preprocessor=dict(
+        type="DetDataPreprocessor",
+        pad_size_divisor=32,
+        batch_augments=[
+            dict(
+                type="BatchSyncRandomResize",
+                random_size_range=(480, 800),
+                size_divisor=32,
+                interval=10,
+            )
+        ],
+    ),
+    backbone=dict(
+        type="ELANDarknet",
+        deepen_factor=2,
+        widen_factor=1,
+        out_indices=(2, 3, 4),
+        act_cfg=dict(type=activation),
+    ),
+    neck=dict(
+        type="YOLOXPAFPN_ELAN",
+        in_channels=[128, 256, 512],
+        out_channels=128,
+        num_elan_blocks=2,
+        act_cfg=dict(type=activation),
+    ),
+    bbox_head=dict(
+        type="YOLOXHead",
+        num_classes=40,
+        in_channels=128,
+        feat_channels=128,
+        act_cfg=dict(type=activation),
+        loss_cls=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=0.0),
+        loss_bbox=dict(type='IoULoss', loss_weight=0.0),
+        loss_obj=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=0.0),
+        loss_l1=dict(type='L1Loss', loss_weight=0.0),   
+    ),
+    mask_head=dict(
+        type="YOLOXSegHead",
+        in_channels=[128, 128, 128],
+        feat_channels=128,
+        num_classes=40,
+        act_cfg=dict(type=activation),
+        loss=dict(type="CrossEntropyLoss", use_sigmoid=False, loss_weight=1.0),
+    ),
+    train_cfg=dict(assigner=dict(type="SimOTAAssigner", center_radius=2.5)),
+    test_cfg=dict(score_thr=0.01, nms=dict(type="nms", iou_threshold=0.65)),
+)
+
+data_root = ""
+anno_file_root = "./data/comlops/semseg/"
+dataset_type = "T4Dataset"
+
+backend_args = None
+
+# pipeline
+train_pipeline = [
+    # dict(type="Mosaic", img_scale=IMG_SCALE, pad_val=114.0),
+    # dict(
+    #     type="RandomAffine",
+    #     scaling_ratio_range=(0.1, 2),
+    #     border=(-IMG_SCALE[0] // 2, -IMG_SCALE[1] // 2),
+    # ),
+    # dict(type="MixUp", img_scale=IMG_SCALE, ratio_range=(0.8, 1.6), pad_val=114.0),
+    dict(type="LoadAnnotations", with_bbox=True, with_seg=True),
+    dict(type="YOLOXHSVRandomAug"),
+    dict(type="RandomFlip", prob=0.5),
+    dict(type="Resize", scale=IMG_SCALE, keep_ratio=False),
+    dict(
+        type="Pad",
+        pad_to_square=True,
+        pad_val=dict(img=(114.0, 114.0, 114.0), seg=255),
+    ),
+    dict(type="FilterAnnotations", min_gt_bbox_wh=(1, 1), keep_empty=False),
+    dict(type="PackDetInputs"),
+]
+
+classes = (
+    "animal",
+    "bicycle",
+    "building",
+    "bus",
+    "car",
+    "cone",
+    "construction",
+    "crosswalk",
+    "dashed_lane_marking",
+    "deceleration_line",
+    "gate",
+    "guide_post",
+    "laneline_dash_white",
+    "laneline_dash_yellow",
+    "laneline_solid_green",
+    "laneline_solid_red",
+    "laneline_solid_white",
+    "laneline_solid_yellow",
+    "marking_arrow",
+    "marking_character",
+    "marking_other",
+    "motorcycle",
+    "other_obstacle",
+    "other_pedestrian",
+    "other_vehicle",
+    "parking_lot",
+    "pedestrian",
+    "pole",
+    "road",
+    "road_debris",
+    "sidewalk",
+    "sky",
+    "stopline",
+    "striped_road_marking",
+    "traffic_light",
+    "traffic_sign",
+    "train",
+    "truck",
+    # "unknown",
+    "vegetation/terrain",
+    "wall/fence",
+)
+
+palette = [
+    (150, 120, 90),   # 0: animal
+    (119, 11, 32),    # 1: bicycle
+    (70, 70, 70),     # 2: building
+    (0, 60, 100),     # 3: bus 
+    (0, 0, 142),      # 4: car
+    (250, 170, 30),   # 5: cone 
+    (230, 150, 140),  # 6: construction 
+    (140, 140, 200),  # 7: crosswalk 
+    (255, 255, 255),  # 8: dashed_lane_marking
+    (200, 200, 200),  # 9: deceleration_line 
+    (190, 153, 153),  # 10: gate 
+    (250, 170, 30),   # 11: guide_post
+    (255, 255, 255),  # 12: laneline_dash_white
+    (255, 255, 0),    # 13: laneline_dash_yellow
+    (0, 255, 0),      # 14: laneline_solid_green
+    (255, 0, 0),      # 15: laneline_solid_red
+    (255, 255, 255),  # 16: laneline_solid_white
+    (255, 215, 0),    # 17: laneline_solid_yellow 
+    (0, 255, 255),    # 18: marking_arrow
+    (200, 0, 200),    # 19: marking_character 
+    (150, 0, 150),    # 20: marking_other
+    (0, 0, 230),      # 21: motorcycle 
+    (80, 80, 80),     # 22: other_obstacle
+    (250, 170, 160),  # 23: other_pedestrian 
+    (100, 80, 200),   # 24: other_vehicle 
+    (180, 165, 180),  # 25: parking_lot
+    (220, 20, 60),    # 26: pedestrian 
+    (153, 153, 153),  # 27: pole 
+    (128, 64, 128),   # 28: road
+    (110, 110, 110),  # 29: road_debris
+    (244, 35, 232),   # 30: sidewalk
+    (70, 130, 180),   # 31: sky 
+    (220, 220, 220),  # 32: stopline 
+    (160, 150, 180),  # 33: striped_road_marking
+    (250, 170, 30),   # 34: traffic_light
+    (220, 220, 0),    # 35: traffic_sign
+    (0, 80, 100),     # 36: train 
+    (0, 0, 70),       # 37: truck 
+    (107, 142, 35),   # 38: vegetation/terrain
+    (102, 102, 156),  # 39: wall/fence
+]
+metainfo = dict(classes=classes, palette=palette)
+
+train_dataset = dict(
+    type="MultiImageMixDataset",
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file=anno_file_root + "comlops_infos_train_cleaned.json",
+        pipeline=[
+            dict(type="LoadImageFromFile", backend_args=backend_args),
+            dict(type="LoadAnnotations", with_bbox=True, with_seg=True),
+        ],
+        filter_cfg=dict(filter_empty_gt=False, min_size=8),
+        backend_args=backend_args,
+        metainfo=metainfo,
+    ),
+    pipeline=train_pipeline,
+)
+
+test_pipeline = [
+    dict(type="LoadImageFromFile", backend_args=backend_args),
+    dict(type="LoadAnnotations", with_bbox=True, with_seg=True),
+    dict(type="Resize", scale=img_scale, keep_ratio=False),
+    dict(type="Pad", pad_to_square=True, pad_val=dict(img=(114.0, 114.0, 114.0), seg=255)),
+    dict(
+        type="PackDetInputs",
+        meta_keys=(
+            "img_id",
+            "img_path",
+            "ori_shape",
+            "img_shape",
+            "scale_factor",
+        ),
+    ),
+]
+
+train_dataloader = dict(
+    batch_size=batch_size,
+    num_workers=num_workers,
+    persistent_workers=True,
+    sampler=dict(type="DefaultSampler", shuffle=True),
+    dataset=train_dataset,
+)
+
+val_dataloader = dict(
+    batch_size=batch_size,
+    num_workers=16,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type="DefaultSampler", shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file=anno_file_root + "comlops_infos_val_cleaned.json",
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args,
+        metainfo=metainfo,
+        indices=2000, 
+    ),
+)
+
+test_dataloader = val_dataloader
+
+val_evaluator = [
+    dict(type="VOCMetric", metric="mAP", prefix="det"),
+    dict(type='mmseg.IoUMetric', ignore_index=255, iou_metrics=['mIoU'], prefix="seg")
+]
+
+test_evaluator = val_evaluator
+
+# train_cfg = dict(max_epochs=max_epochs, val_interval=interval)
+train_cfg = dict(
+    _delete_=True,
+    type='IterBasedTrainLoop',
+    max_iters=200000,     
+    val_interval=1000
+)
+
+# optimizer
+optimizer = dict(
+    type="OptimWrapper",
+    optimizer=dict(type="SGD", lr=base_lr, momentum=0.9, weight_decay=5e-4, nesterov=True),
+    paramwise_cfg=dict(norm_decay_mult=0.0, bias_decay_mult=0.0),
+)
+
+# learning rate scheduler
+if max_epochs > 5:
+    param_scheduler = [
+        dict(
+            type="mmdet.QuadraticWarmupLR",
+            by_epoch=True,
+            begin=0,
+            end=5,
+            convert_to_iter_based=True,
+        ),
+        dict(
+            type="CosineAnnealingLR",
+            eta_min=base_lr * 0.05,
+            begin=5,
+            T_max=max_epochs - num_last_epochs,
+            end=max_epochs - num_last_epochs,
+            by_epoch=True,
+            convert_to_iter_based=True,
+        ),
+        dict(
+            type="ConstantLR",
+            by_epoch=True,
+            factor=1,
+            begin=max_epochs - num_last_epochs,
+            end=max_epochs,
+        ),
+    ]
+else:
+    param_scheduler = []
+
+# logging
+log_config = dict(
+    interval=1,
+    hooks=[dict(type="TextLoggerHook"), dict(type="TensorboardLoggerHook")],
+)
+
+# default_hooks = dict(
+#     checkpoint=dict(interval=interval, max_keep_ckpts=3),
+# )
+default_hooks = dict(
+    checkpoint=dict(
+        type='CheckpointHook', 
+        interval=1000,
+        by_epoch=False,
+        max_keep_ckpts=5, 
+        save_best='seg/mIoU',
+        rule='greater'
+    ),
+    logger=dict(
+        type='LoggerHook', 
+        interval=50 
+    ),
+    visualization=dict(
+        type='DetVisualizationHook',
+        draw=False,
+        interval=100,
+        show=False,
+        wait_time=2,
+        test_out_dir='vis_data'
+    ),
+)
+
+custom_hooks = [
+    dict(type="YOLOXModeSwitchHook", num_last_epochs=num_last_epochs, priority=48),
+    dict(type="SyncNormHook", priority=48),
+    dict(
+        type="EMAHook",
+        ema_type="ExpMomentumEMA",
+        momentum=0.0001,
+        update_buffers=True,
+        priority=4,
+    ),
+]
+
+auto_scale_lr = dict(base_batch_size=batch_size)
+
+
+vis_backends = [
+    dict(type="LocalVisBackend"),
+    dict(type="TensorboardVisBackend"),
+]
+
+visualizer = dict(
+    type='DetLocalVisualizer',
+    vis_backends=[dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend')],
+    name='visualizer',
+    alpha=0.3,
+)
---
+++ projects/YOLOX_opt_elan/yolox/models/yolox_multitask.py
@@ -0,0 +1,189 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch import Tensor
+
+from mmengine.model import BaseModule
+from mmdet.registry import MODELS
+from mmdet.models import BaseDetector
+from mmdet.utils import ConfigType, OptConfigType, OptMultiConfig
+from mmdet.structures import DetDataSample
+from mmengine.structures import PixelData, InstanceData
+from mmdet.structures import SampleList
+
+from .heads import YOLOXSegHead
+
+from mmengine.logging import print_log
+
+@MODELS.register_module()
+class YOLOXMultiTask(BaseDetector):
+    """
+    YOLOX MultiTask detector
+    Supports bbox + mask heads.
+    """
+
+    def __init__(self,
+                 backbone,
+                 neck,
+                 bbox_head,
+                 mask_head=None,
+                 train_cfg: OptConfigType = None,
+                 test_cfg: OptConfigType = None,
+                 data_preprocessor=None,
+                 init_cfg=None,
+                 **kwargs):
+        super().__init__(init_cfg=init_cfg)
+        self.backbone = MODELS.build(backbone)
+        self.neck = MODELS.build(neck) if neck is not None else None
+        if bbox_head is not None:
+            bbox_head.update(train_cfg=train_cfg)
+            bbox_head.update(test_cfg=test_cfg)
+            self.bbox_head = MODELS.build(bbox_head)
+        if mask_head is not None:
+            mask_head.update(train_cfg=train_cfg)
+            mask_head.update(test_cfg=test_cfg)
+            self.mask_head = MODELS.build(mask_head) if mask_head is not None else None
+        self.data_preprocessor = MODELS.build(data_preprocessor) if data_preprocessor else None
+
+    def extract_feat(self, inputs):
+        x = self.backbone(inputs)
+        if self.neck is not None:
+            x = self.neck(x)
+        return x
+
+    def _forward(self, imgs, **kwargs):
+        return self.forward(imgs, **kwargs)
+    
+    def forward_train(self, imgs, gt_bboxes, gt_labels, gt_masks=None, **kwargs):
+        feats = self.extract_feat(imgs)
+        losses = dict()
+        losses.update(self.bbox_head.loss(feats[-1], gt_bboxes, gt_labels))
+        if self.mask_head is not None and gt_masks is not None:
+            mask_pred = self.mask_head(feats)
+            losses.update(self.mask_head.loss(mask_pred, gt_masks))
+        return losses
+
+    def forward_test(self, imgs, **kwargs):
+        feats = self.extract_feat(imgs)
+        bbox_results = self.bbox_head(feats[-1])
+        mask_results = None
+        if self.mask_head is not None:
+            mask_results = self.mask_head(feats)
+        return dict(bboxes=bbox_results, masks=mask_results)
+
+    def forward(self, inputs, data_samples=None, mode='tensor'):
+        """Forward function with training and testing mode."""
+        feats = self.extract_feat(inputs)
+
+        if mode == 'tensor':
+            return self.bbox_head(feats)
+        elif mode == 'loss':
+            s = self.loss(feats, data_samples)
+            return  s 
+        elif mode == 'predict':
+            pred_instances = self.predict(inputs, data_samples)
+    
+            for pred, data_sample in zip(pred_instances, data_samples):
+                pred.gt_instances = data_sample.gt_instances
+                if hasattr(data_sample, 'gt_sem_seg'):
+                    pred.gt_sem_seg = data_sample.gt_sem_seg
+
+            return pred_instances
+        else:
+            raise ValueError(f"Invalid mode {mode}")
+
+    def loss(self, feats, data_samples):
+        loss = dict()
+        # bbox head forward
+        cls_scores, bbox_preds, objectnesses = self.bbox_head(feats)
+        batch_gt_instances = [d.gt_instances for d in data_samples]
+        batch_img_metas = [d.metainfo for d in data_samples]
+
+        loss.update(
+            self.bbox_head.loss_by_feat(
+                cls_scores, bbox_preds, objectnesses, batch_gt_instances, batch_img_metas
+            )
+        )
+
+        # mask head
+        if self.mask_head is not None:
+            seg_pred = self.mask_head(feats)
+            target_size = data_samples[0].gt_sem_seg.sem_seg.shape[-2:]
+            if seg_pred.shape[-2:] != target_size:
+                seg_pred = F.interpolate(seg_pred, size=target_size, mode='bilinear', align_corners=False)
+
+            gt_masks_tensor = []
+            gt_masks = torch.stack([d.gt_sem_seg.sem_seg.squeeze(0) for d in data_samples], dim=0)  # (B, H, W)
+            gt_masks = gt_masks.to(seg_pred.device)
+
+            mask_loss_dict = self.mask_head.loss(seg_pred, gt_masks)
+            for k, v in mask_loss_dict.items():
+                if torch.is_tensor(v):
+                    loss[k] = v
+                else:
+                    raise TypeError(f"mask loss '{k}' is not a tensor")
+
+        return loss
+
+    def predict(self,
+                batch_inputs: Tensor,
+                batch_data_samples: SampleList,
+                rescale: bool = True,
+                **kwargs) -> SampleList:
+        
+        x = self.extract_feat(batch_inputs)
+        
+        if self.with_bbox:
+            bbox_results_list = self.bbox_head.predict(x, batch_data_samples, rescale=True)
+        else:
+            bbox_results_list = [InstanceData() for _ in batch_data_samples]
+
+        seg_results_list = None
+        if self.with_mask:
+            seg_results_list = self.mask_head.predict(x, batch_data_samples, rescale=True)
+
+        results = []
+        for i, data_sample in enumerate(batch_data_samples):
+            data_sample.pred_instances = bbox_results_list[i]
+            
+            if seg_results_list is not None:
+                pixel_data = PixelData()
+                pixel_data.data = seg_results_list[i]
+                pixel_data.sem_seg = seg_results_list[i]
+                data_sample.pred_sem_seg = pixel_data
+            
+            img_h, img_w = data_sample.metainfo['img_shape']
+            ori_h, ori_w = data_sample.metainfo['ori_shape']
+            
+            if hasattr(data_sample, 'gt_instances'):
+
+                scale_factor = data_sample.metainfo['scale_factor'] # (w_scale, h_scale)
+
+                scale_factor_bbox = [scale_factor[0], scale_factor[1], scale_factor[0], scale_factor[1]]
+                scale_tensor = data_sample.gt_instances.bboxes.new_tensor(scale_factor_bbox)
+
+                data_sample.gt_instances.bboxes = data_sample.gt_instances.bboxes / scale_tensor
+
+            if hasattr(data_sample, 'gt_sem_seg') and data_sample.gt_sem_seg is not None:
+                gt_sem_seg_data = data_sample.gt_sem_seg.sem_seg # [H_pad, W_pad]
+                
+                gt_valid = gt_sem_seg_data[..., :img_h, :img_w] 
+
+                if gt_valid.shape[-2:] != (ori_h, ori_w):
+                    gt_resized = F.interpolate(
+                        gt_valid.unsqueeze(0).float(), # [1, 1, h, w]
+                        size=(ori_h, ori_w), 
+                        mode='nearest'
+                    ).squeeze(0).long()
+                    
+                    new_gt_pixel_data = PixelData()
+                    new_gt_pixel_data.sem_seg = gt_resized
+                    new_gt_pixel_data.data = gt_resized
+                    data_sample.gt_sem_seg = new_gt_pixel_data
+                elif 'data' not in data_sample.gt_sem_seg:
+                    data_sample.gt_sem_seg.data = data_sample.gt_sem_seg.sem_seg
+
+            results.append(data_sample)
+
+        return results
