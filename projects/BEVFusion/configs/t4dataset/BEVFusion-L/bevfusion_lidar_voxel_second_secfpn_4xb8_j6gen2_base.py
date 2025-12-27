c958b92d34584c4266b2e2e96c0c017d1f34751c
KokSeang <kseangtan@google.com>
2025-12-27 22:08:56 +0900

    Add

--- projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_4xb8_j6gen2_base.py
+++ projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_4xb8_j6gen2_base.py
@@ -115,9 +115,8 @@ train_pipeline = [
     dict(type="BEVFusionRandomFlip3D"),
     dict(type="PointsRangeFilter", point_cloud_range=point_cloud_range),
     dict(type="ObjectRangeFilter", point_cloud_range=point_cloud_range),
-    dict(type="ObjectRangeMinPointsFilter", range_radius=[0, 60], min_num_points=5),
-    dict(type="ObjectRangeMinPointsFilter", range_radius=[60, 90], min_num_points=3),
-    dict(type="ObjectRangeMinPointsFilter", range_radius=[90, 130], min_num_points=1),
+    dict(type="ObjectRangeMinPointsFilter", range_radius=[0, 60], min_num_points=2),
+    dict(type="ObjectRangeMinPointsFilter", range_radius=[60, 130], min_num_points=1),
     dict(
         type="ObjectNameFilter",
         classes=[
@@ -362,4 +361,4 @@ auto_scale_lr = dict(enable=False, base_batch_size=train_gpu_size * train_batch_
 if train_gpu_size > 1:
     sync_bn = "torch"
 
-load_from = "work_dirs/bevfusion_2_3/T4Dataset/bevfusion_lidar_voxel_second_secfpn_4xb16_base/epoch_46.pth"
+load_from = "work_dirs/bevfusion_2_5/T4Dataset/bevfusion_lidar_base/epoch_50.pth"
