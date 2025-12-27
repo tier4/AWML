41b0464cbbba19a053dda644755e782c8d1b6d86
KokSeang <kseangtan@google.com>
2025-12-27 22:05:29 +0900

    Add

--- projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_4xb8_j6gen2_base.py
+++ projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_4xb8_j6gen2_base.py
@@ -115,6 +115,9 @@ train_pipeline = [
     dict(type="BEVFusionRandomFlip3D"),
     dict(type="PointsRangeFilter", point_cloud_range=point_cloud_range),
     dict(type="ObjectRangeFilter", point_cloud_range=point_cloud_range),
+    dict(type="ObjectRangeMinPointsFilter", range_radius=[0, 60], min_num_points=5),
+    dict(type="ObjectRangeMinPointsFilter", range_radius=[60, 90], min_num_points=3),
+    dict(type="ObjectRangeMinPointsFilter", range_radius=[90, 130], min_num_points=1),
     dict(
         type="ObjectNameFilter",
         classes=[
