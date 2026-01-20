bef622853f2dd5c5b7a3a4d3c8c3847543aa8ba7
Kok Seang Tan <49808895+KSeangTan@users.noreply.github.com>
2026-01-20 19:00:45 +0900

    chore(BEVFusion): restructure configs in BEVFusion (#170)
    Co-authored-by: KokSeang <kseangtan@google.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- projects/BEVFusion/configs/t4dataset/default/models/default_lidar_second_secfpn_120m.py
+++ projects/BEVFusion/configs/t4dataset/default/models/default_lidar_second_secfpn_120m.py
@@ -1,47 +1,25 @@
-_base_ = ["../../../../../autoware_ml/configs/detection3d/default_runtime.py"]
-
-# range setting
-point_cloud_range = [-14.4, -14.4, -3.0, 14.4, 14.4, 5.0]
-voxel_size = [0.01, 0.01, 0.01]
-grid_size = [1440, 1440, 41]
-eval_class_range = {
-    "car": 120,
-    "truck": 120,
-    "bus": 120,
-    "bicycle": 120,
-    "pedestrian": 120,
-}
-
-# model parameter
-input_modality = dict(use_lidar=True, use_camera=False)
-point_load_dim = 5  # x, y, z, intensity, ring_id
-point_use_dim = 5
-point_intensity_dim = 3
+num_proposals = 500
 max_num_points = 10
 max_voxels = [120000, 160000]
-num_proposals = 500
 
 model = dict(
     type="BEVFusion",
+    voxelize_cfg=dict(
+        max_num_points=max_num_points,
+        max_voxels=max_voxels,
+        voxelize_reduce=True,
+    ),
     data_preprocessor=dict(
         type="Det3DDataPreprocessor",
         pad_size_divisor=32,
-        voxelize_cfg=dict(
-            max_num_points=max_num_points,
-            point_cloud_range=point_cloud_range,
-            voxel_size=voxel_size,
-            max_voxels=max_voxels,
-            voxelize_reduce=True,
-        ),
     ),
-    pts_voxel_encoder=dict(type="HardSimpleVFE", num_features=5),
+    pts_voxel_encoder=dict(type="HardSimpleVFE"),
     pts_middle_encoder=dict(
         type="BEVFusionSparseEncoder",
         in_channels=5,
         aug_features_min_values=[],
         aug_features_max_values=[],
         num_aug_features=0,
-        sparse_shape=grid_size,
         order=("conv", "norm", "act"),
         norm_cfg=dict(type="BN1d", eps=0.001, momentum=0.01),
         encoder_channels=((16, 16, 32), (32, 32, 64), (64, 64, 128), (128, 128)),
@@ -91,9 +69,6 @@ model = dict(
         ),
         train_cfg=dict(
             dataset="t4datasets",
-            point_cloud_range=point_cloud_range,
-            grid_size=grid_size,
-            voxel_size=voxel_size,
             out_size_factor=8,
             gaussian_overlap=0.1,
             min_radius=2,
@@ -109,10 +84,7 @@ model = dict(
         ),
         test_cfg=dict(
             dataset="t4datasets",
-            grid_size=grid_size,
             out_size_factor=8,
-            voxel_size=voxel_size[0:2],
-            pc_range=point_cloud_range[0:2],
             nms_type=None,  # Set to "circle" for circle_nms
             # Set NMS for different clusters
             nms_clusters=[
@@ -125,8 +97,6 @@ model = dict(
         common_heads=dict(center=[2, 2], height=[1, 2], dim=[3, 2], rot=[2, 2], vel=[2, 2]),
         bbox_coder=dict(
             type="TransFusionBBoxCoder",
-            pc_range=point_cloud_range[0:2],
-            voxel_size=voxel_size[0:2],
             post_center_range=[-200.0, -200.0, -10.0, 200.0, 200.0, 10.0],
             score_threshold=0.0,
             out_size_factor=8,
@@ -144,18 +114,3 @@ model = dict(
         loss_bbox=dict(type="mmdet.L1Loss", reduction="mean", loss_weight=0.25),
     ),
 )
-
-vis_backends = [
-    dict(type="LocalVisBackend"),
-    dict(type="TensorboardVisBackend"),
-]
-visualizer = dict(type="Det3DLocalVisualizer", vis_backends=vis_backends, name="visualizer")
-
-default_hooks = dict(
-    logger=dict(type="LoggerHook", interval=50),
-    checkpoint=dict(type="CheckpointHook", interval=1, max_keep_ckpts=3, save_best="NuScenes metric/T4Metric/mAP"),
-)
-custom_hooks = [dict(type="DisableObjectSampleHook", disable_after_epoch=15)]
-log_processor = dict(window_size=50)
-
-randomness = dict(seed=0, diff_rank_seed=True, deterministic=True)
