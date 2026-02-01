73ea06b8770a608f163a375e7e35c65db6a61f89
KokSeang Tan <kseangtan@Gmail.com>
2026-02-01 12:36:34 +0900

    Update configs

--- projects/CenterPoint/configs/t4dataset/Centerpoint/second_secfpn_4xb16_121m_jpntaxi_base_amp.py
+++ projects/CenterPoint/configs/t4dataset/Centerpoint/second_secfpn_4xb16_121m_jpntaxi_base_amp.py
@@ -50,7 +50,7 @@ num_workers = 32
 val_interval = 1
 max_epochs = 30
 
-experiment_group_name = "centerpoint_2.5.1/jpntaxi_base/" + _base_.dataset_type
+experiment_group_name = "centerpoint_2.6.1/jpntaxi_base/" + _base_.dataset_type
 experiment_name = "second_secfpn_4xb16_121m_jpntaxi_base_amp"
 work_dir = "work_dirs/" + experiment_group_name + "/" + experiment_name
 
