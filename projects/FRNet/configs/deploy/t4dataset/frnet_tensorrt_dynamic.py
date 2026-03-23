a01a9303217e8ef40e8b571168c0b52b38b9499b
Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>
2026-03-23 12:11:06 +0900

    fix(FRNet): remove wrong profile & update profiles
    Signed-off-by: Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>

--- projects/FRNet/configs/deploy/t4dataset/frnet_tensorrt_dynamic.py
+++ projects/FRNet/configs/deploy/t4dataset/frnet_tensorrt_dynamic.py
@@ -9,11 +9,10 @@ custom_imports = dict(
 
 num_classes = _base_.num_classes
 tensorrt_config = dict(
-    points=dict(min_shape=[5000, 4], opt_shape=[60000, 4], max_shape=[120000, 4]),
-    coors=dict(min_shape=[5000, 3], opt_shape=[60000, 3], max_shape=[120000, 3]),
+    points=dict(min_shape=[5000, 4], opt_shape=[60000, 4], max_shape=[160000, 4]),
+    coors=dict(min_shape=[5000, 3], opt_shape=[60000, 3], max_shape=[160000, 3]),
     voxel_coors=dict(min_shape=[3000, 3], opt_shape=[30000, 3], max_shape=[60000, 3]),
-    inverse_map=dict(min_shape=[5000], opt_shape=[60000], max_shape=[120000]),
-    pred_probs=dict(min_shape=[5000, num_classes], opt_shape=[60000, num_classes], max_shape=[120000, num_classes]),
+    inverse_map=dict(min_shape=[5000], opt_shape=[60000], max_shape=[160000]),
 )
 
 onnx_config = dict(
