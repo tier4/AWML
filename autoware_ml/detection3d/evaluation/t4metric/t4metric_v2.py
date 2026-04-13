9ee9962ff6bb9e140b60caa8b25c5e0bb0e3192b
Maxime CLEMENT <maxime.clement@tier4.jp>
2026-04-13 15:18:35 +0900

    update t4metric_v2 to support None locations (for lidarseg)
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- autoware_ml/detection3d/evaluation/t4metric/t4metric_v2.py
+++ autoware_ml/detection3d/evaluation/t4metric/t4metric_v2.py
@@ -1311,8 +1311,12 @@ class T4MetricV2(BaseMetric):
         # Extract evaluation annotation info for the current sample
         eval_info: dict = data_sample.get("eval_ann_info", {})
         sample_id: str = data_sample.get("sample_idx", _UNKNOWN)
-        location: str = data_sample.get("city", _UNKNOWN)
-        vehicle_type: str = data_sample.get("vehicle_type", _UNKNOWN)
+        location: str = data_sample.get("city")
+        if location is None:
+            location = _UNKNOWN
+        vehicle_type: str = data_sample.get("vehicle_type")
+        if vehicle_type is None:
+            vehicle_type = _UNKNOWN
 
         # gt_bboxes_3d: LiDARInstance3DBoxes with tensor of shape (N, 9)
         # Format per box: [x, y, z, l, w, h, yaw, vx, vy]
