1a9cb6f59e38274fa02aa789e3799652908a3678
KSeangTan <kseangtan@gmail.com>
2026-04-18 17:49:37 +0900

    Update config dataset name

--- projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_30e_8xb8_j6gen2_base_120m_t4metric_v2.py
+++ projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_30e_8xb8_j6gen2_base_120m_t4metric_v2.py
@@ -3,9 +3,6 @@ _base_ = [
 ]
 
 # user setting
-data_root = "data/t4dataset/"
-info_directory_path = "info/user_name/"
-
 experiment_group_name = "bevfusion_lidar_intensity_2.6.1/j6gen2_base/" + _base_.dataset_type
 experiment_name = "lidar_voxel_second_secfpn_30e_8xb8_j6gen2_base_120m_t4metric_v2"
 work_dir = "work_dirs/" + experiment_group_name + "/" + experiment_name
