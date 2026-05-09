be69b11cb6732c96d5ae185db7b5c6521a65708d
Kok Seang Tan <kseangtan@gmail.com>
2026-05-09 17:03:29 +0900

    Resolve conflict

--- projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_50e_8xb16_base_120m_sincos_48_channels_32_points.py
+++ projects/BEVFusion/configs/t4dataset/BEVFusion-L/bevfusion_lidar_voxel_second_secfpn_50e_8xb16_base_120m_sincos_48_channels_32_points.py
@@ -3,7 +3,7 @@ _base_ = [
     "../../../../../autoware_ml/configs/detection3d/dataset/t4dataset/base.py",
     "../default/pipelines/default_lidar_120m.py",
     "../default/models/default_lidar_second_secfpn_120m.py",
-    "../default/schedulers/default_50e_8xb8_adamw_cosine.py",
+    "../default/schedulers/default_50e_8xb16_adamw_cosine.py",
     "../default/default_misc.py",
 ]
 
