3af4270f9259f2ba16e422a83f701d3064c04706
Kok Seang Tan <kseangtan@gmail.com>
2026-05-29 19:10:37 +0900

    Updated

--- autoware_ml/detection3d/datasets/transforms/__init__.py
+++ autoware_ml/detection3d/datasets/transforms/__init__.py
@@ -1,3 +1,4 @@
 from .object_min_points_filter import ObjectMinPointsFilter
+from .local_3d_bbox import Local3DBBoxExpand
 
-__all__ = ["ObjectMinPointsFilter"]
+__all__ = ["ObjectMinPointsFilter", "Local3DBBoxExpand"]
