862e72be298f85108b1ce7e86926180d3edbe3ec
KSeangTan <kseangtan@gmail.com>
2026-06-21 18:21:37 +0900

    Remove uncessary config changes

--- projects/BEVFusion/bevfusion/__init__.py
+++ projects/BEVFusion/bevfusion/__init__.py
@@ -1,7 +1,7 @@
 from .bevfusion import BEVFusion
 from .bevfusion_head import BEVFusionHead, ConvFuser
 from .bevfusion_necks import GeneralizedLSSFPN
-from .bevfusion_voxel_encoder import BEVFusionVoxelFeatureNet, HardSimpleVoxelSinCosEncoder
+from .bevfusion_voxel_encoder import HardSimpleVoxelSinCosEncoder
 from .depth_lss import DepthLSSTransform, LSSTransform
 from .loading import BEVLoadMultiViewImageFromFiles
 from .sparse_encoder import BEVFusionSparseEncoder
@@ -28,6 +28,5 @@ __all__ = [
     "BEVFusionRandomFlip3D",
     "BEVFusionGlobalRotScaleTrans",
     "TransFusionBBoxCoder",
-    "HardSimpleVoxelSinCosEncoder",
-    "BEVFusionVoxelFeatureNet",
+    "HardSimpleVoxelSinCosEncoder"
 ]
