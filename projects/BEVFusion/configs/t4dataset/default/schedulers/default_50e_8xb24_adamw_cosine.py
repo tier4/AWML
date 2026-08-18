19fd5f4680f560f3f2678b9b7ff53a644c22ee5f
Kok Seang Tan <kseangtan@gmail.com>
2026-08-18 15:06:20 +0900

    Update configs

--- projects/BEVFusion/configs/t4dataset/default/schedulers/default_50e_8xb24_adamw_cosine.py
+++ projects/BEVFusion/configs/t4dataset/default/schedulers/default_50e_8xb24_adamw_cosine.py
@@ -10,7 +10,7 @@ train_batch_size = 24
 
 param_scheduler = [
     # learning rate scheduler
-    # During the first (max_epochs * 0.10) epochs, learning rate increases from 0 to lr * 5
+    # During the first (max_epochs * 0.10) epochs, learning rate increases from 0 to lr * 10
     # during the next epochs, learning rate decreases from lr * 10 to
     # lr * 1e-4
     dict(
@@ -32,7 +32,7 @@ param_scheduler = [
         convert_to_iter_based=True,
     ),
     # momentum scheduler
-    # During the first (max_epochs * 0.10) epochs, momentum increases from 0 to 0.85 / 0.95
+    # During the first (0.10 * max_epochs) epochs, momentum increases from 0 to 0.85 / 0.95
     # during the next epochs, momentum increases from 0.85 / 0.95 to 1
     dict(
         type="CosineAnnealingMomentum",
